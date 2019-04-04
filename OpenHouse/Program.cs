556a9e7d98b51d97378daf254d998639d3414795
terrell oliver <mroliver1995@aol.com>
2019-04-03 22:22:45 -0500

    Added cleaner code

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -23,102 +23,86 @@ namespace OpenHouse
             {
                 return false;
             }
-        }   
-            
-        
+        }
+
+
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome! Thank you for coming to my open house today!");
             string name = GetUserResponse("Whats your name?");
-            Console.WriteLine("It's so nice to eet you, " + name + ".");                   
-                               
-
-            
+            Console.WriteLine("It's so nice to eet you, " + name + ".");
 
             string answer = GetUserResponse("Would you like to see the house?");
 
-            
+            if (answer == "yes")
+            {
 
-                             if (answer == "yes")
-                              {
-                                
-                                //ShowRoom(livingRoom);
-                                //ShowRoom(diningRoom);
-                                //ShowRoom(kitchen);
-                                 Console.WriteLine("The house has an upstaris and a downstaris area!");
-
-                              }
-                              else
-                               {
-                                 
-                                 Console.WriteLine("Oh dang, well it was very nice to meet you!");
-                                 Console.ReadLine();
-                
-                              }
-
-                                 Console.WriteLine("Would you like to take a one-sheet?");
-                                 answer = Console.ReadLine();
-
-                                 if (answer == "yes")
-                                  {
-
-                                    Console.WriteLine("Here you go!");
-
-                                  }
-                                 else
-                                  {
-                                  Console.WriteLine("Alright, have a good day!");
-                                  }
-
-                                  Console.WriteLine("Would you like to see the kitchen?");
-
-                                  if (IsAffirmative(answer))
-
-                            Console.WriteLine("The kitchen comes with a refrigerator that has its own build in ice machine!");
-                            Console.WriteLine("Would you like to see the living room?");
-                            Console.WriteLine("The living room conatins a nice pool set table!");
-
-                            if (answer == "yes")
-                            {
-                            // show the living room
-                            }
-                            else
-                            {
-                            Console.WriteLine("Ok, well we will go to the next area!");
-                            } 
-                
-                            Console.WriteLine("Would you like to go upstaris?");
-                            Console.WriteLine("Upstairs has a major gameroom such as xbox, playstation, and nintendo!");
-                            Console.WriteLine("Its the perfect place for someone who wants competition or to just have fun!");
-
-                            if (IsAffirmative(answer))
-                            {
-                            // show the upstaris
-                            }
-                            else
-                            {
-                            Console.WriteLine("Ok, lets go to the next area!");
-                            } 
-            
-                            
-                                
-                            
-
-                                 
-
-
-
-
-
-
-                         Console.ReadLine();
-                
-
-            
-            
+                //ShowRoom(livingRoom);
+                //ShowRoom(diningRoom);
+                //ShowRoom(kitchen);
+                Console.WriteLine("The house has an upstaris and a downstaris area!");
 
             }
+            else
+            {
+
+                Console.WriteLine("Oh dang, well it was very nice to meet you!");
+                Console.ReadLine();
+
+            }
+
+            Console.WriteLine("Would you like to take a one-sheet?");
+            answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+
+                Console.WriteLine("Here you go!");
+
+            }
+            else
+            {
+                Console.WriteLine("Alright, have a good day!");
+            }
+
+            Console.WriteLine("Would you like to see the kitchen?");
+
+            if (IsAffirmative(answer))
+
+            Console.WriteLine("The kitchen comes with a refrigerator that has its own build in ice machine!");
+            Console.WriteLine("Would you like to see the living room?");
+            Console.WriteLine("The living room conatins a nice pool set table!");
+
+            if (answer == "yes")
+            {
+                // show the living room
+            }
+            else
+            {
+                Console.WriteLine("Ok, well we will go to the next area!");
+            }
+
+            Console.WriteLine("Would you like to go upstaris?");
+            Console.WriteLine("Upstairs has a major gameroom such as xbox, playstation, and nintendo!");
+            Console.WriteLine("Its the perfect place for someone who wants competition or to just have fun!");
+
+            if (IsAffirmative(answer))
+            {
+                // show the upstaris
+            }
+            else
+            {
+                Console.WriteLine("Ok, lets go to the next area!");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
         }
     }
+}
 
